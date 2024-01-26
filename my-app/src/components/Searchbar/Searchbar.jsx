7de211d57a456fc29aa3a1d9200e1b4a91e1e8da
SanikaC1999 <131801208+SanikaC1999@users.noreply.github.com>
2024-01-26 16:19:42 +0530

    navbar code updated

--- my-app/src/components/Searchbar/Searchbar.jsx
+++ my-app/src/components/Searchbar/Searchbar.jsx
@@ -7,7 +7,7 @@ export default function Searchbar(){
     return (
         <>
         <form className={styles.Searchbar}>
-            <input placeholder="Search a ablum of your choice"  className={styles.search}/>
+            <input placeholder="search a ablum of your choice"  className={styles.search}/>
             <button type="submit" className={styles.searchbutton}>
                 <img src={searchboximg} alt="searchimg" width={20} height={20}/>
             </button>
