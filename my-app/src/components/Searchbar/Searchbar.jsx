1c6a50f9473a31fd8756a101b5e8cfb558079ad1
SanikaC1999 <131801208+SanikaC1999@users.noreply.github.com>
2024-01-26 16:46:18 +0530

    search css upadte

--- my-app/src/components/Searchbar/Searchbar.jsx
+++ my-app/src/components/Searchbar/Searchbar.jsx
@@ -7,7 +7,7 @@ export default function Searchbar(){
     return (
         <>
         <form className={styles.Searchbar}>
-            <input placeholder="search a ablum of your choice"  className={styles.search}/>
+            <input placeholder="search a ablum of your choice"  className={styles.search} required/>
             <button type="submit" className={styles.searchbutton}>
                 <img src={searchboximg} alt="searchimg" width={20} height={20}/>
             </button>
