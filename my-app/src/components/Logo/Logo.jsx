f3cc7a4729f5468e081b26a752f7d901aaeccc84
SanikaC1999 <131801208+SanikaC1999@users.noreply.github.com>
2024-01-26 16:24:48 +0530

    logo styling upfated

--- my-app/src/components/Logo/Logo.jsx
+++ my-app/src/components/Logo/Logo.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 import logoimg from "../../assets/logoimg.png"
+import styles from "./Logo.module.css"
 
 export default function Logo(){
     return (
         <>
-        <img src={logoimg} alt="logo" width = {67} className="logoimage"/>
+        <img src={logoimg} alt="logo" width = {67} className={styles.logoimage}/>
         </>
     )
 }
