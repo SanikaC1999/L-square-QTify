969c5e12882290a725a96ecbbdc67e4ae533dc6c
SanikaC1999 <131801208+SanikaC1999@users.noreply.github.com>
2024-01-27 18:12:01 +0530

    navbar updated again

--- my-app/src/components/Searchbar/Searchbar.jsx
+++ my-app/src/components/Searchbar/Searchbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import searchboximg from "../../assets/Search icon.svg"
 import styles from "./searchbar.module.css"
 
-export default function Searchbar(search){
+export default function Searchbar({search}){
     return (
         <>
         <form className={styles.Searchbar}>
